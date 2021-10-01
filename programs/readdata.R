5e3f7477d02742e8c034bb068a445810fb541611
Wendy Mack <wmack@usc.edu>
2021-10-01 13:19:43 -0700

    Added max age variable line

--- programs/readdata.R
+++ programs/readdata.R
@@ -2,3 +2,5 @@ library(tidyverse)
 library(dplyr)
 demo<-read.cvs("d:/my work/naps/analysis subprojects/athero and rheology/data/persinfo.cvs")
 head(demo)
+#   add for branch example
+maxage<-max(demo$age)
